5dd81a955d38eb36bbb9f4cefe8259c90225c19e
shwetachoudhary06 <shweta.choudhary@asu.edu>
2016-02-01 18:16:24 -0700

    Update_Feb1

--- Assignment1/app/src/main/java/cse535/asu/com/assignment1/MainActivity.java
+++ Assignment1/app/src/main/java/cse535/asu/com/assignment1/MainActivity.java
@@ -4,7 +4,9 @@ import java.util.Random;
 
 import android.graphics.Color;
 import android.os.Bundle;
+
 import java.util.Random;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +27,11 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.GridLabelRenderer;
+
+/**
+ *
+ */
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,11 +55,10 @@ public class MainActivity extends AppCompatActivity {
         graph.getGridLabelRenderer().setVerticalAxisTitle("Rate");
         graph.setTitle("Health Monitoring");
         graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.HORIZONTAL);
-        final String[][] str = {{"Alice","83","1"},{"Bob","7","0"},{"Mallory", "42","2"},{"Carol", "43", "1"},{"Dave", "91", "0"},{"Eve", "66", "1"},{"Walter", "77", "0"},{"Peggy", "65","2"},{"Trent", "21","0"},{"Peggy", "71","2"}};
+        final String[][] str = {{"Alice", "83", "1"}, {"Bob", "7", "0"}, {"Mallory", "42", "2"}, {"Carol", "43", "1"}, {"Dave", "91", "0"}, {"Eve", "66", "1"}, {"Walter", "77", "0"}, {"Peggy", "65", "2"}, {"Trent", "21", "0"}, {"Peggy", "71", "2"}};
 
 
-
-        Button startButton = (Button)findViewById(R.id.startbutton);
+        Button startButton = (Button) findViewById(R.id.startbutton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 String name = editText.getText().toString();
                 EditText editText01 = (EditText) findViewById(R.id.EditText01);
                 Integer id = Integer.parseInt(editText01.getText().toString());
-                EditText editText02= (EditText) findViewById(R.id.EditText02);
+                EditText editText02 = (EditText) findViewById(R.id.EditText02);
                 String age = editText02.getText().toString();
 
                 RadioButton male = (RadioButton) findViewById(R.id.male);
@@ -83,20 +89,18 @@ public class MainActivity extends AppCompatActivity {
                 int genderId = gender.getCheckedRadioButtonId();
                 Integer gen;
 
-                if(genderId==male.getId()){
-                    gen=0;
-                }
-                else if(genderId==female.getId()){
-                    gen=1;
-                }
-                else{
-                    gen=2;
+                if (genderId == male.getId()) {
+                    gen = 0;
+                } else if (genderId == female.getId()) {
+                    gen = 1;
+                } else {
+                    gen = 2;
                 }
                 String input_name = name;
                 String input_age = age;
                 int input_gender = gen;
 
-                if(id<=0 || id>10||str[id-1][0].equals(input_name)==false || str[id-1][1].equals(input_age)==false || Integer.parseInt(str[id-1][2])!=input_gender){
+                if (id <= 0 || id > 10 || str[id - 1][0].equals(input_name) == false || str[id - 1][1].equals(input_age) == false || Integer.parseInt(str[id - 1][2]) != input_gender) {
                     alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
@@ -108,13 +112,12 @@ public class MainActivity extends AppCompatActivity {
                     graph.removeAllSeries();
                     alertDialog.show();
 
-                }
-                else {
+                } else {
 
-                        GraphView graph = (GraphView) findViewById(R.id.graph);
-                    graph.setTitle("Health Data for" + name);
-                    graph.setBackgroundColor(Color.argb(60, 255, 0, 255 ));
-                    graph.setTitleColor(Color.MAGENTA);
+                    GraphView graph = (GraphView) findViewById(R.id.graph);
+                    graph.setTitle("Health Data for " + name);
+                    graph.setBackgroundColor(Color.argb(60, 255, 0, 255));
+                    graph.setTitleColor(Color.BLACK);
                     graph.getViewport().setScalable(true);
                     graph.getViewport().setScrollable(true);
                     graph.getViewport().setXAxisBoundsManual(true);
@@ -122,35 +125,29 @@ public class MainActivity extends AppCompatActivity {
                     graph.getViewport().setMaxX(120);
 
 
-                        //GridLabelRenderer(graph);
+                    //GridLabelRenderer(graph);
 
 
+                    double data;
+                    //System.out.println("name: " + name + " age: " + age + " id: " + id + " gender: " + genderId);
 
+                    double graph2LastXValue = 0d;
 
+                    Random generator = new Random(id);
+                    LineGraphSeries<DataPoint> mSeries2 = new LineGraphSeries<DataPoint>();
 
-                        double data;
-                        System.out.println("name: " + name + " age: " + age + " id: " + id + " gender: " + genderId);
+                    for (int i = 0; i < 100; i++) {
+                        data = generator.nextDouble() * 0.5;
+                        mSeries2.appendData(new DataPoint(graph2LastXValue, data), true, 101);
+                        graph2LastXValue += 1d;
 
-                        double graph2LastXValue = 0d;
-
-                        Random generator = new Random(id);
-                        LineGraphSeries<DataPoint> mSeries2 = new LineGraphSeries<DataPoint>();
-
-                        for (int i = 0; i < 100; i++) {
-                            data = generator.nextDouble() * 0.5;
-                            mSeries2.appendData(new DataPoint(graph2LastXValue, data), true, 101);
-                            graph2LastXValue += 1d;
-
-                        }
+                    }
                     graph.addSeries(mSeries2);
-                    mSeries2.setColor(Color.MAGENTA);
+                    mSeries2.setColor(Color.BLACK);
                     mSeries2.setBackgroundColor(Color.GRAY);
-                    mSeries2.setThickness(2);
-                    mSeries2.setDrawBackground(true);
-                    mSeries2.setDrawBackground(true);
+                    mSeries2.setThickness(5);
+                    //mSeries2.setDrawBackground(true);
                     mSeries2.setDataPointsRadius(10);
-
-
 
 
                 }
@@ -158,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        Button stopButton = (Button)findViewById(R.id.stopbutton);
+        Button stopButton = (Button) findViewById(R.id.stopbutton);
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
 
@@ -169,7 +166,6 @@ public class MainActivity extends AppCompatActivity {
                 graph.removeAllSeries();
             }
         });
-
 
 
     }
@@ -199,6 +195,7 @@ public class MainActivity extends AppCompatActivity {
     private double getRandom() {
         double mLastRandom = 2;
         Random mRand = new Random();
-        return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
+        return mLastRandom += mRand.nextDouble() * 0.5 - 0.25;
     }
 }
+
