27af6e1df001bd799f0f51537b3c471df2aab79e
Prajwal Paudyal Home <ppaudyal@asu.edu>
2016-02-28 14:58:53 -0700

    some database work

--- Assignment2/app/src/main/java/cse535/asu/com/assignment2/MainActivity.java
+++ Assignment2/app/src/main/java/cse535/asu/com/assignment2/MainActivity.java
@@ -1,32 +1,42 @@
 package cse535.asu.com.assignment2;
 
-import java.util.Random;
-
-import android.graphics.Color;
-import android.os.Bundle;
-import java.util.Random;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.animation.Animation;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.RadioGroup;
-import android.widget.RadioButton;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.GridLabelRenderer;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    //Global variables
+    private final String DATABASE_NAME = "Patient_Info_Database";
+    private String DATABASE_LOCATION, SDCARD_LOCATION;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +53,11 @@ public class MainActivity extends AppCompatActivity {
         //           .setAction("Action", null).show();
         // }
         //});
+
+        //Things for initializing graphs
+        SDCARD_LOCATION = Environment.getExternalStorageDirectory().getPath();
+        DATABASE_LOCATION = SDCARD_LOCATION + "/Assignment2DB";
+
         GraphView graph = (GraphView) findViewById(R.id.graph);
         graph.getGridLabelRenderer().setHorizontalAxisTitle("Time");
         graph.getGridLabelRenderer().setVerticalAxisTitle("Rate");
@@ -50,7 +65,10 @@ public class MainActivity extends AppCompatActivity {
         graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.HORIZONTAL);
         final String[][] str = {{"Alice","83","1"},{"Bob","7","0"},{"Mallory", "42","2"},{"Carol", "43", "1"},{"Dave", "91", "0"},{"Eve", "66", "1"},{"Walter", "77", "0"},{"Peggy", "65","2"},{"Trent", "21","0"},{"Peggy", "71","2"}};
 
-
+        //register the sensors
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener()
 
         Button startButton = (Button)findViewById(R.id.startbutton);
         startButton.setOnClickListener(new View.OnClickListener() {
@@ -181,6 +199,60 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //This get the accls values and puts in in the database
+    public void startAcclService(View view){
+
+        //put all this in a method in a thread
+        mSensorManager.registerListener(this, mAccelerometer, 1000000);
+        x = y = z = new double[100];
+        //graphing related init
+        /*graphView = new LineGraphView(this, "GraphViewDemo");
+        gvdx = new GraphViewData[10];
+        gvdy = new GraphViewData[10];
+        gvdz = new GraphViewData[10];*/
+       /* batchSize = 10;
+        updatedAcclValues = new double[3];
+        tableCreated = false;*/
+        //uploadComplete = false;
+
+        //get database location
+        SDCARD_LOCATION = Environment.getExternalStorageDirectory().getPath();
+        DATABASE_LOCATION = SDCARD_LOCATION + "/Assignment2DB";
+
+
+        /*get table name*/
+        //creating a database
+        //createDatabase();
+
+        //compose the table name
+        tableName = "A" + patientNameEdit.getText().toString()
+                + "_" + patientIdEdit.getText().toString()
+                + "_" + ageEdit.getText().toString()
+                + "_" + radioSexButton.getText().toString();
+        SQLiteDatabase.openDatabase(DATABASE_LOCATION, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        //SQLiteDatabase.openDatabase("/sdcard/Assigment2DB", null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        createTable();
+        //readyToWrite = true;
+        //enabling Run Button
+
+        //disable StartBtn -- after implementing a reset functionality
+        startAcclBtn.setEnabled(false);
+
+        Toast.makeText(this, "waiting for db update", Toast.LENGTH_LONG).show();
+
+    }
+
+
+    //This function is the one that 'starts the graph' **This might not be needed. If not needed for
+    //final implementation, move the logic to the onclick listener.
+    public void initializeGraph(){
+
+
+
+    }
+
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -200,5 +272,66 @@ public class MainActivity extends AppCompatActivity {
         double mLastRandom = 2;
         Random mRand = new Random();
         return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void writeToTable(double[] xArray, double[] yArray, double[] zArray) {
+        //
+        //databaseLocked = true;
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(DATABASE_LOCATION, null, SQLiteDatabase.OPEN_READWRITE);
+        Log.i("writeTable", "in WriteTable");
+
+        for (int counter = 0; counter < xArray.length; counter++){
+            db.beginTransaction();
+            try{//make table name global
+                db.execSQL("insert into " + tableName + "(xValues, yValues, xValues) values ("+ (double)xArray[counter] + ", " + (double)yArray[counter] + ", " + (double)zArray[counter] + ");");
+                //Log.i("DatabaseWriter", "Writing" + xObject[counter] + ", " + yObjects[counter] + ", " + zObjects[counter] + ");" );
+                //db.execSQL(INSERT_DUMMY_VALUES_SQL);
+                db.setTransactionSuccessful();
+                //databasePopulatedFlag = true;
+            }catch(SQLiteException e){
+                Log.i("Database", e.getMessage());
+            }finally{
+                db.endTransaction();
+            }
+        }
+        db.close();
+
+    }
+
+    public void createTable(){
+        //create the database
+
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(DATABASE_LOCATION, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        Log.i("createTable", "in CreateTable");
+        String CREATE_TABLE_SQL = "create table if not exists " + tableName + " ("
+                + "timeStamp integer PRIMARY KEY autoincrement, "
+                + "xValues double, "
+                + "yValues double, "
+                + "zValues double ); ";
+
+        //String INSERT_DUMMY_VALUES_SQL = "insert into " + tableName + "(xValues, yValues, xValues) values (12, 13, 14);";
+
+        db.beginTransaction();
+        try{
+            db.execSQL(CREATE_TABLE_SQL);
+            //db.execSQL(INSERT_DUMMY_VALUES_SQL);
+            db.setTransactionSuccessful();
+        }catch(SQLiteException e){
+            Log.i("Database", e.getMessage());
+        }finally{
+            db.endTransaction();
+            tableCreated = true;
+            db.close();
+        }
     }
 }
