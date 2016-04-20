7e41fabcd95d6a1f017ac933201968b86e8c7ef0
Shibani Singh <shibanisingh7866@gmail.com>
2016-04-20 13:13:46 -0700

    Login working, Upload working, Receiving json encoded data

--- TheAlphabets/TheAlphabets/src/main/java/com/cse535/thealphabets/TestActivity.java
+++ TheAlphabets/TheAlphabets/src/main/java/com/cse535/thealphabets/TestActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.Menu;
@@ -41,18 +42,41 @@ import java.security.cert.X509Certificate;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+import android.os.Environment;
+
 
 public class TestActivity extends Activity {
 
     private DeviceListener mListener = new AbstractDeviceListener(){};
     private Button testButton;
-    private String DATABASE_LOCATION, SDCARD_LOCATION;
+    String DATABASE_LOCATION, SDCARD_LOCATION;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        SDCARD_LOCATION = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        if (android.os.Build.DEVICE.contains("samsung")
+                || android.os.Build.MANUFACTURER.contains("samsung")) {
+            File f = new File(Environment.getExternalStorageDirectory()
+                    .getParent() + "/extSdCard" + "/myDirectory");
+            if (f.exists() && f.isDirectory()) {
+                SDCARD_LOCATION = Environment.getExternalStorageDirectory()
+                        .getParent() + "/extSdCard";
+            } else {
+                f = new File(Environment.getExternalStorageDirectory()
+                        .getAbsolutePath() + "/external_sd" + "/myDirectory");
+                if (f.exists() && f.isDirectory()) {
+                    SDCARD_LOCATION = Environment
+                            .getExternalStorageDirectory().getAbsolutePath()
+                            + "/external_sd";
+                }
+            }
+        }
 
         /*Spinner dropdown = (Spinner) findViewById(R.id.alphabets);
         String[] items = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "9"};
