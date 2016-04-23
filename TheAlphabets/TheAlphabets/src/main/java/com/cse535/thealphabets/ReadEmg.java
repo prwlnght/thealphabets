9a44417258330a91f2e54d9d17bc0c4ccd9ad882
shwetachoudhary06 <shweta.choudhary@asu.edu>
2016-04-23 15:07:41 -0700

    Myolib Integration

--- TheAlphabets/TheAlphabets/src/main/java/com/cse535/thealphabets/ReadEmg.java
+++ TheAlphabets/TheAlphabets/src/main/java/com/cse535/thealphabets/ReadEmg.java
@@ -4,6 +4,16 @@ package com.cse535.thealphabets;
 import android.util.Log;
 
 import java.util.ArrayList;
+import eu.darken.myolib.BaseMyo;
+import eu.darken.myolib.Myo;
+import eu.darken.myolib.MyoCmds;
+import eu.darken.myolib.MyoConnector;
+import eu.darken.myolib.msgs.MyoMsg;
+import eu.darken.myolib.processor.emg.EmgData;
+import eu.darken.myolib.processor.emg.EmgProcessor;
+import eu.darken.myolib.processor.imu.ImuData;
+import eu.darken.myolib.processor.imu.ImuProcessor;
+
 
 public class ReadEmg  {
 
